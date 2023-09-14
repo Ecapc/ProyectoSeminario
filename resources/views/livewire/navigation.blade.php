9e363f2d47496f99909e57f63d2c2c4f932b811f
Emerson Andres Perez Chen <emersonandresperezchen@PcEmerson.local>
2023-09-13 20:37:14 -0600

    PantallaPrincipal

---
+++ resources/views/livewire/navigation.blade.php
@@ -0,0 +1,139 @@
+<header class="bg-gray-600 sticky top-0 z-50" x-data="dropdown()">
+    <div class="container flex items-center h-16 justify-between md:justify-start">
+        <a :class="{ '!bg-opacity-100 !text-white': open }" x-on:click="show()"
+            class="flex flex-col items-center justify-center order-last md:order-first px-6 md:px-4 bg-gray-900 bg-opacity-25 text-white cursor-pointer h-full">
+            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                <path lass="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+            <span class="text-sm hidden md:block">
+                Menu
+            </span>
+        </a>
+        <a href="/dashboard" class="mx-6">
+            <x-application-mark class="block h-9 w-auto" />
+        </a>
+        <div class="flex-1 hidden md:block">
+            @livewire('search')
+        </div>
+
+        <!-- Menu de logeo -->
+        <div>
+            @auth
+                <div class="hidden sm:flex sm:items-center sm:ml-6 md:block">
+                    <!-- Teams Dropdown -->
+                    @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
+                        <div class="ml-3 relative">
+                            <x-dropdown align="right" width="60">
+
+                                <x-slot name="content">
+                                    <div class="w-60">
+                                        <!-- Team Management -->
+                                        <div class="block px-4 py-2 text-xs text-gray-400">
+                                            {{ __('Manage Team') }}
+                                        </div>
+
+                                        <!-- Team Settings -->
+                                        <x-dropdown-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}">
+                                            {{ __('Team Settings') }}
+                                        </x-dropdown-link>
+
+                                        @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
+                                            <x-dropdown-link href="{{ route('teams.create') }}">
+                                                {{ __('Create New Team') }}
+                                            </x-dropdown-link>
+                                        @endcan
+
+                                        <!-- Team Switcher -->
+                                        @if (Auth::user()->allTeams()->count() > 1)
+                                            <div class="border-t border-gray-200 dark:border-gray-600"></div>
+
+                                            <div class="block px-4 py-2 text-xs text-gray-400">
+                                                {{ __('Switch Teams') }}
+                                            </div>
+
+                                            @foreach (Auth::user()->allTeams() as $team)
+                                                <x-switchable-team :team="$team" />
+                                            @endforeach
+                                        @endif
+                                    </div>
+                                </x-slot>
+                            </x-dropdown>
+                        </div>
+                    @endif
+
+                    <!-- Settings Dropdown -->
+                    <div class="ml-3 relative">
+                        <x-dropdown algn="right" width="48">
+                            <x-slot name="trigger">
+                                @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                                    <button
+                                        class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                        <img class="h-8 w-8 rounded-full object-cover"
+                                            src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                    </button>
+                                @else
+                                    <span class="inline-flex rounded-md">
+                                        <button type="button"
+                                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                                            {{ Auth::user()->name }}
+
+                                            <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                            </svg>
+                                        </button>
+                                    </span>
+                                @endif
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <!-- Account Management -->
+                                <div class="block px-4 py-2 text-xs text-gray-400">
+                                    {{ __('Manage Account') }}
+                                </div>
+
+                                <x-dropdown-link href="{{ route('profile.show') }}">
+                                    {{ __('Profile') }}
+                                </x-dropdown-link>
+
+                                <div class="border-t border-gray-200 dark:border-gray-600"></div>
+
+                                <!-- Authentication -->
+                                <form method="POST" action="{{ route('logout') }}" x-data>
+                                    @csrf
+
+                                    <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
+                                        {{ __('Log Out') }}
+                                    </x-dropdown-link>
+                                </form>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
+                </div>
+            @else
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <i class="fas fa-user-circle text-white text-3xl cursor-pointer items-center justify-center px-4">
+                        </i>
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <x-dropdown-link href="{{ route('login') }}">
+                            {{ __('Login') }}
+                        </x-dropdown-link>
+
+                        <x-dropdown-link href="{{ route('register') }}">
+                            {{ __('Register') }}
+                        </x-dropdown-link>
+                    </x-slot>
+                </x-dropdown>
+            @endauth
+        </div>
+         <!-- Fin Menu de logeo -->
+
+         
+    </div>
+    
+</header>
---
+++ resources/views/livewire/search.blade.php
@@ -0,0 +1,8 @@
+<div class="flex-1 relative">
+    <x-input type="text" class="w-full" placeholder="¿Que es lo que buscas?" />
+    <button class="absolute top-0 right-0 w-12 h-full bg-blueGray-500 flex items-center justify-center rounded-r-md">
+        <x-search-logo size="35" color="white"/>
+    </button>
+
+</div>
+
